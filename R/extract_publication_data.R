09a781323dd85d802812bef12c5121f6e15b57b8
Ryan MacGregor <117998563+RMac4@users.noreply.github.com>
2024-04-30 21:12:19 +0100

    more commentary added

--- R/extract_publication_data.R
+++ R/extract_publication_data.R
@@ -21,8 +21,7 @@
 #'
 #' @returns The names for each sheets as per the contents page, the data tables
 #' and the notes page. Note that shorthand has been recoded to -1 for suppressed 
-#' rows, -2 for not applicable and -3 for low. Dependent on the publication,
-#' percentage values may be multiplied by 100
+#' rows, -2 for not applicable and -3 for low.
 #' 
 #' @import dplyr stringr
 #' @importFrom openxlsx read.xlsx
@@ -40,20 +39,22 @@ extract_publication_data <- function(file_source = "web",
                                      file_name = NA_character_,
                                      custom_path = NA_character_){
   
-  # initial validation checks ----
-  ## check valid source option has been input
+  # input validation checks ----
+  ## check valid file source option has been input, if not tell user to check
   stopifnot('Must be either "web" or "custom". If "custom" provide the file path' = 
               file_source %in% c("web", "custom"))
   
-  ## check file name has been provided
+  ## check valid file name has been provided, if not tell user to check
   stopifnot('"file_name" has not been provided, please provide the file name of the excel document' = 
               is.na(file_name) == FALSE)
   
+  # link and file path validation checks for web source ----
   if (file_source == "web"){
     ## source link from web
     link <- file.path("https://www.sqa.org.uk/sqa/files_ccc", file_name)
     
-    ## check if file exists, if not ask to check file name
+    ## check if file exists on web
+    ## if file does not exist, stop code and tell user to check file name
     if (httr::status_code(httr::HEAD(link)) == 200){
       link
     }else{
@@ -61,15 +62,19 @@ extract_publication_data <- function(file_source = "web",
                  paste0('"', file_name,'"')))
     }
     
-  }else if (file_source == "custom"){
-    # check path has been provided
+  }
+  
+  # link and file path validation checks for custom source ----
+  if (file_source == "custom"){
+    ## check a custom file path has been provided when custom option selected
     stopifnot('"custom_path" has not been provided, please provide the file path to excel location' = 
                 is.na(custom_path) == FALSE)
     
     ## source link from custom path
     link <- file.path(custom_path, file_name)
     
-    ## check file exists, if not ask to check file path and file name
+    ## check file exists in custom path
+    ## if file does not exist, stop code and tell user to check file path and name
     if (file.exists(link)){
       link
     }else{
@@ -90,20 +95,24 @@ extract_publication_data <- function(file_source = "web",
     dplyr::filter(stringr::str_detect(sheets, "Table"))
   
   # extract the notes sheet ----
+  ## notes sheet is always the last sheet in publications, add two to number of
+  ## tables to account for contents page
   notes <- openxlsx::read.xlsx(
     xlsxFile = link,
     sheet = (nrow(sheets) + 2),
     startRow = 2
     )
   
-  # check correct number of sheets ----
+  # expected tables ----
+  ## tell user the number of tables to be extracted and what they should be
   cat(paste("There should be",
             nrow(sheets),
             "tables extracted from this publication:\n"), 
-      paste(sheets$sheets, "\n")
-      )
+      paste(sheets$sheets, "\n"))
   
   # set start row ----
+  ## look at cell A2 in first data table sheet for specific text used for 
+  ## shorthand note identify start of data table
   start_row <- dplyr::if_else(
     openxlsx::read.xlsx(
       xlsxFile = link,
@@ -126,9 +135,13 @@ extract_publication_data <- function(file_source = "web",
   
   # check correct number of tables ----
   if(length(tables_raw) == nrow(sheets)){
-    cat("\nThere were", length(tables_raw), "tables extracted from this publication.\n")
+    ## note to user that the correct number of tables have been extracted
+    cat("\nThere were", 
+        length(tables_raw), 
+        "tables extracted from this publication.\n")
   }else{
-    stop("Number of data tables extracted does not match expected number of tables.")
+    ## stops code if the number of tables extracted does not match expected output
+    stop("Number of tables extracted does not match expected number of tables.")
   }
   
   # replace shorthand and convert to number ----  
@@ -139,7 +152,7 @@ extract_publication_data <- function(file_source = "web",
       dplyr::summarise_all(~any(grepl("%", .))) |> 
       dplyr::select(dplyr::where(~. == TRUE)) |> 
       colnames()
-      )
+    )
   
   ## replace suppressed values with -1, not applicable with -2 and low with -3
   ## if percentage symbol is present, remove and values should be divided by 100
@@ -164,11 +177,13 @@ extract_publication_data <- function(file_source = "web",
         ### change percentages to their decimal value
         dplyr::across(
           dplyr::matches(percent_flag[[.x]]),
-          ~ ifelse(. < 0, ., . / 100)
+          ~ifelse(. < 0, ., . / 100)
           )
         ) |>
+      ### tidy column names
       dplyr::rename_with(.fn = ~stringr::str_replace_all(., "[.]", "_"))
     ) |>
+    ## set name for each element of list to associated table number
     setNames(
       sheets |> 
         dplyr::mutate(
@@ -180,6 +195,7 @@ extract_publication_data <- function(file_source = "web",
         dplyr::pull(sheets)
       )
   
+  ## text to notify user that shorthand values have been replaced
   cat("\nShorthand has been replaced as follows:\n[c] with -1 (suppressed),\n[z] with -2 (not applicable),\n[low] with -3")
   
   # function returns ----
